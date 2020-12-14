f86ef509a8f1ef7a116c5d6589560b46dfeee813
SaibUmea <74403281+SaibUmea@users.noreply.github.com>
2020-12-14 21:00:46 +0100

    nnnnn

---
+++ Saib_Maluk/src/uppgift_2/Main.java
@@ -0,0 +1,10 @@
+package uppgift_2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Now 14Dec at night");
+	}
+
+}
---
+++ Saib_Maluk/src/uppgift_2/package-info.java
@@ -0,0 +1,1 @@
+package uppgift_2;
